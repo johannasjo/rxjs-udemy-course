2daf126f35225da71c04a7071f78448ca5c3f260
johannasjo <64302410+johannasjo@users.noreply.github.com>
2023-02-09 11:55:24 +0100

    cold observable

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import './style.css';
 
 import { Observable } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 const interval$ = new Observable<number>((subscriber) => {
   let counter = 1;
@@ -21,3 +22,9 @@ setTimeout(() => {
   console.log('unsubscribe');
   subscription.unsubscribe();
 }, 10000);
+
+// example of cold observable
+const nameReq$ = ajax<any>('https://random-data-api.com/api/name/random_name');
+nameReq$.subscribe((randomName) => {
+  console.log(randomName.response?.first_name);
+});
